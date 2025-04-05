de80bd1adb0388adebd1c43a1e26e6adb338ded9
IotaSphere <40618095+Geocube101@users.noreply.github.com>
2025-04-04 21:19:51 -0400

    Added CM6

--- EmployeeProfile.py
+++ EmployeeProfile.py
@@ -1,2 +1,12 @@
+import CustomMethodsVI.Parser.KVP as KVP
+
+
 class MyEmployeeProfile:
-    pass
+    def __init__(self, kvp: KVP.KVP):
+        self.__employee_name__: str = kvp.EmployeeProfile.Name
+        self.__skills__: list[str] = kvp.EmployeeProfile.Skills
+        self.__project_ids__: list[int] = kvp.EmployeeProfile.ProjectIDs
+        self.__performance_review_ids__: list[int] = kvp.EmployeeProfile.PerfReviewIDs
+        self.__assessment_ids: list[int] = kvp.EmployeeProfile.AssessmentIDs
+        self.__role__: str = kvp.EmployeeProfile.CurrentRole
+
