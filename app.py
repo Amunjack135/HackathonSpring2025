3dca1bd33896f10f8420d4a38de3fbcb19728d42
IotaSphere <40618095+Geocube101@users.noreply.github.com>
2025-04-04 20:46:52 -0400

    Initial Files

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import flask
+import time
 
 import CustomMethodsVI.Connection as Connection
 
 app: flask.Flask = flask.Flask(__name__)
+socketio: Connection.FlaskSocketioServer = Connection.FlaskSocketioServer(app)
 
 
-@app.route("/")
+@app.route('/ncathack')
 def index():
     return flask.render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    try:
+        socketio.async_listen(port=443)
+        print(f'Server started @ {socketio.host}:{socketio.port}')
+
+        while not socketio.closed:
+            time.sleep(1)
+    except (SystemExit, KeyboardInterrupt):
+        print('Server closed.')
+        socketio.close()
